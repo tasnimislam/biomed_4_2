6c5a8cb6bc879b39f36d0fb3d007ae14e83bb653
tasnimislam <46325413+tasnimislam@users.noreply.github.com>
2023-02-03 14:07:22 +0600

    Binary classifier results checked

---
+++ image_processing.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import cv2
+import numpy as np
+
+def image_preprocessing(path):
+    image = Image.open(path)
+    image = np.array(image)
+
+    # Preprocessing I have decided
+    ad_mean = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    _, otsu = cv2.threshold(ad_mean, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, bin_cann = cv2.threshold(otsu, 127, 255, cv2.THRESH_BINARY)
+    # colored = cv2.cv2.cvtColor(bin_cann,cv2.COLOR_GRAY2RGB)
+    # resized_image = cv2.resize(colored, (32, 32), interpolation=cv2.INTER_AREA)
+    return bin_cann / 255.0
